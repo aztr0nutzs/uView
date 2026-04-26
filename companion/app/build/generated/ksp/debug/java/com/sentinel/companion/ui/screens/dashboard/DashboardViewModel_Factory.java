2f85f3c3ce330657e417789765a1346f2c711728
aztr0nutzs <aztr0nutzs@users.noreply.github.com>
2026-04-26 19:33:19 -0400

    Updates Gradle build metadata and state
    Refreshes internal Gradle state files, including execution history and file hashes, to synchronize the repository with the current build environment.

--- companion/app/build/generated/ksp/debug/java/com/sentinel/companion/ui/screens/dashboard/DashboardViewModel_Factory.java
+++ companion/app/build/generated/ksp/debug/java/com/sentinel/companion/ui/screens/dashboard/DashboardViewModel_Factory.java
@@ -1,6 +1,8 @@
 package com.sentinel.companion.ui.screens.dashboard;
 
 import com.sentinel.companion.data.repository.CameraRepository;
+import com.sentinel.companion.data.repository.DeviceRepository;
+import com.sentinel.companion.data.repository.PreferencesRepository;
 import dagger.internal.DaggerGenerated;
 import dagger.internal.Factory;
 import dagger.internal.QualifierMetadata;
@@ -23,22 +25,33 @@ import javax.inject.Provider;
     "cast"
 })
 public final class DashboardViewModel_Factory implements Factory<DashboardViewModel> {
-  private final Provider<CameraRepository> repoProvider;
+  private final Provider<CameraRepository> cameraRepoProvider;
 
-  public DashboardViewModel_Factory(Provider<CameraRepository> repoProvider) {
-    this.repoProvider = repoProvider;
+  private final Provider<DeviceRepository> deviceRepoProvider;
+
+  private final Provider<PreferencesRepository> prefsRepoProvider;
+
+  public DashboardViewModel_Factory(Provider<CameraRepository> cameraRepoProvider,
+      Provider<DeviceRepository> deviceRepoProvider,
+      Provider<PreferencesRepository> prefsRepoProvider) {
+    this.cameraRepoProvider = cameraRepoProvider;
+    this.deviceRepoProvider = deviceRepoProvider;
+    this.prefsRepoProvider = prefsRepoProvider;
   }
 
   @Override
   public DashboardViewModel get() {
-    return newInstance(repoProvider.get());
+    return newInstance(cameraRepoProvider.get(), deviceRepoProvider.get(), prefsRepoProvider.get());
   }
 
-  public static DashboardViewModel_Factory create(Provider<CameraRepository> repoProvider) {
-    return new DashboardViewModel_Factory(repoProvider);
+  public static DashboardViewModel_Factory create(Provider<CameraRepository> cameraRepoProvider,
+      Provider<DeviceRepository> deviceRepoProvider,
+      Provider<PreferencesRepository> prefsRepoProvider) {
+    return new DashboardViewModel_Factory(cameraRepoProvider, deviceRepoProvider, prefsRepoProvider);
   }
 
-  public static DashboardViewModel newInstance(CameraRepository repo) {
-    return new DashboardViewModel(repo);
+  public static DashboardViewModel newInstance(CameraRepository cameraRepo,
+      DeviceRepository deviceRepo, PreferencesRepository prefsRepo) {
+    return new DashboardViewModel(cameraRepo, deviceRepo, prefsRepo);
   }
 }
